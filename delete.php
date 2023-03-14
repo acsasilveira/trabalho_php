a68a0bfd701b2ec1510c1b01c7c82076a7df17e0
acsasilveira <acsa.silveiras@gmail.com>
2023-03-14 15:13:57 -0300

    Versao 14/03 - Acsa

--- delete.php
+++ delete.php
@@ -1,5 +1,24 @@
 <?php
+require_once 'init.php';
+$id = isset($_GET['id']) ? $_GET['id'] : null;
+if(empty($id))
+{
+    echo "ID não informado";
+    exit;
+}
 
-
+$PDO = db_connect();
+$sql = "DELETE FROM Series WHERE id = :id";
+$stmt = $PDO->prepare($sql);
+$stmt->bindParam(':id', $id, PDO::PARAM_INT);
+if($stmt -> execute())
+{
+    header('Location: index.php');
+}
+else
+{
+    echo "Erro ao remover";
+    print_r($stmt->errorInfo());
+}
 
 ?>
---
+++ edit.php
@@ -0,0 +1,35 @@
+<?php
+require_once 'init.php';
+$nome = isset($_POST['nome']) ? $_POST['name'] : null;
+$canal = isset($_POST['canal']) ? $_POST['canal'] : null;
+$ano = isset($_POST['ano']) ? $_POST['ano'] : null;
+$temporadas = isset($_POST['temporadas']) ? $_POST['temporadas'] : null;
+$avaliacao = isset($_POST['avaliacao']) ? $_POST['avaliacao'] : null;
+if ((empty($nome)) || (empty($canal)) || (empty($ano)) || (empty($temporadas)) || (empty($avaliacao)))
+{
+    echo "Por gentilza, preencha todos os campos!";
+    exit;
+}
+if ($avaliacao > 10 || $avaliacao < 0)
+{
+    echo "A avaliação deve ser feita apenas entre 0 a 10!";
+}
+$PDO = db_connect();
+$sql = "UPDATE Series SET nome = :nome, canal = :canal, ano = :ano, temporadas = :temporadas, avaliacao = :avaliacao WHERE id = :id";
+$stmt = $PDO->prepare($sql);
+$stmt->bindParam(':nome', $nome);
+$stmt->bindParam(':canal', $canal);
+$stmt->bindParam(':ano', $ano);
+$stmt->bindParam(':temporadas', $temporadas);
+$stmt->bindParam(':avaliacao', $avaliacao);
+$stmt->bindParam(':id', $id, PDO::PARAM_INT);
+if($stmt -> execute())
+{
+    header('Location: index.php');
+}
+else
+{
+    echo "Erro ao cadastrar";
+    print_r($stmt->errorInfo());
+}
+?>
